0882e7f358138ab4ab3e9af27bec5fa66eaf8d5a
Tom Cobb <tom.cobb@diamond.ac.uk>
2022-02-04 11:57:26 +0000

    Added a commandline parser
    Skeleton's customer forgot how to use the tool, and invoked it with
    --help. The results were not very useful. Skeleton added a commandline
    parser that responds correctly and also prints the version.
    
    Now you can get help:
      pipenv run skeleton-says --help

--- src/skeleton_says/say.py
+++ src/skeleton_says/say.py
@@ -1,4 +1,3 @@
-import sys
 from jinja2 import Template
 from pathlib import Path
 
@@ -7,9 +6,3 @@ template_path = Path(__file__).parent / "skeleton.jinja2"
 def say(text):
     template = Template(template_path.read_text())
     return template.render(text=text)
-
-def main():
-    print(say(sys.argv[1]))
-
-if __name__ == "__main__":
-    main()
