24df70bd6fbef046f5eaa4ae3fafb6a465c88b60
Tom Cobb <tom.cobb@diamond.ac.uk>
2022-02-04 16:22:06 +0000

    Improved coverage
    Skeleton could see now that the commandline tool needed testing as well
    as the library invokation. Adding a couple of tests (and putting in a
    tests directory for neatness) brought the coverage up. z_slide.png shows
    what codecov.io thinks.

---
+++ tests/test_cli.py
@@ -0,0 +1,10 @@
+import sys, subprocess
+from skeleton_says import __version__
+
+def test_cli_version():
+    cmd = [sys.executable, "-m", "skeleton_says", "--version"]
+    assert subprocess.check_output(cmd).decode().strip() == __version__
+
+def test_cli_says_hello():
+    cmd = [sys.executable, "-m", "skeleton_says", "Hello"]
+    assert "Hello" in subprocess.check_output(cmd).decode()
