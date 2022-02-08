1ac2b28d0448907adf4058abbd9b80d155e4679f
thomascobb <thomascobb@users.noreply.github.com>
2022-02-08 11:14:25 +0000

    deploy: e51b2f685e93f2606d840e48f7906ef3200b7da5

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0+untagged.g58fb6eb',
+    VERSION: '0.0+untagged.ge51b2f6',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
